60b7c481c2249fa31b074e914b0da37d7ce0646c
Hotávio Pacheco <8taviop@gmail.com>
2023-04-05 15:14:21 -0300

    parte 4

---
+++ src/user/dtos/updateuser.dto.ts
@@ -0,0 +1,10 @@
+import { MinLength, IsString } from "class-validator";
+import { UserMessagesHelper } from "../helpers/messages.helper";
+
+export class UpdateUserDto {
+    @MinLength(2, {message: UserMessagesHelper.REGISTER_NAME_NOT_VALID})
+    name: string;
+
+    @IsString()
+    avatar: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,33 @@
-import {BadRequestException, Controller, Get, Request} from '@nestjs/common';
-import { UserService } from './user.service';
+import {Controller, Get, Request, BadRequestException, Body, Put, HttpCode, HttpStatus} from '@nestjs/common';
+import { UpdateUserDto } from './dtos/updateuser.dto';
 import { UserMessagesHelper } from './helpers/messages.helper';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController{
-    constructor(private readonly userService:UserService){}
+    constructor(private readonly userSerice:UserService){}
 
     @Get()
     async getUser(@Request() req){
         const {userId} = req?.user;
-        const user = await this.userService.getUserById(userId);
+        const user = await this.userSerice.getUserById(userId);
 
         if(!user){
             throw new BadRequestException(UserMessagesHelper.GET_USER_NOT_FOUND);
         }
 
-        return{
+        return {
             name: user.name,
-            email : user.email,
+            email: user.email,
             avatar: user.avatar,
             id: user._id
         }
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    async updateUser(@Request() req, @Body() dto: UpdateUserDto){
+        const {userId} = req?.user;
+        await this.userSerice.updateUser(userId, dto);
     }
 }
